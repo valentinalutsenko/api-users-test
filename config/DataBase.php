070139de86d9ecd1412b4da4e20aec2b59c432ba
Valentina <lucenkovalentina@yandex.ru>
2024-07-24 12:14:01 +0300

    add connecting db

---
+++ config/DataBase.php
@@ -0,0 +1,23 @@
+<?php
+
+class DataBase
+{
+    private $host = 'localhost';
+    private $database = 'api-users';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    //Creating connection to database
+    public function connect()
+    {
+        $this->conn = null;
+        try{
+            $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->database, 'root', '');
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->conn;
+        }catch(PDOException $e){
+            echo 'Connection error: '.$e->getMessage();
+        }
+    }
+}
